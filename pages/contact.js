63f8bedc56901f0eaeb3d3178e1417afe51595d1
Piotr_Bator <pbator94@gmail.com>
2021-07-05 12:44:34 +0200

    Add google maps.

--- pages/contact.js
+++ pages/contact.js
@@ -8,8 +8,14 @@ import phoneIcon from "../public/icons/icon-tel.png";
 import mailIcon from "../public/icons/icon-mail.png";
 import mapIcon from "../public/icons/icon-map.png";
 import logoIcon from "../public/icons/artpoz-icon.png";
+import { WrappedMap } from "../components/Map/Map";
+import mapStyles from "../styles/Map.module.css";
+import styles from "../styles/Contact.module.css";
 
 export default function Contact() {
+  const defaultMapElement = <div style={{ height: "100%" }} />;
+  const iconSize = 24;
+
   return (
     <div>
       <Head>
@@ -29,80 +35,80 @@ export default function Contact() {
           <Col lg={5} className="mb-4">
             <Card className="shadow-lg">
               <Card.Body>
-                <div className="d-flex align-items-center">
-                  <h5 className="text-uppercase p-0 m-0">
-                    <Image
-                      width={64}
-                      height={64}
-                      alt="Artpoż logo icon"
-                      src={logoIcon}
-                    />
+                <div className={styles.contactHeader}>
+                  <Image
+                    width={64}
+                    height={64}
+                    alt="Artpoż logo icon"
+                    src={logoIcon}
+                  />
+                  <h5 className="text-uppercase m-0 p-0 ml-2">
                     Skontaktuj się z nami
                   </h5>
                 </div>
                 <hr />
-                <div className="card-info">
-                  <div className="card-img">
+                <div className={styles.cardInfo}>
+                  <div className={styles.cardImg}>
                     <Image
                       src={personIcon}
                       alt="Person contact icon"
-                      width={32}
-                      height={32}
+                      width={iconSize}
+                      height={iconSize}
                     />
-                    <h5 className="mb-0">Dane firmy</h5>
+                    <h6 className="mb-0 ml-3">Dane firmy</h6>
                   </div>
                   <p>
-                    <span className="text-bold">Pełna nazwa:</span> PHU ARTPOŻ
-                    Arkadiusz Bątor
+                    <span className={styles.cardTextBold}>Pełna nazwa:</span>{" "}
+                    PHU ARTPOŻ Arkadiusz Bątor
                   </p>
                   <p>
-                    <span className="text-bold">NIP:</span> 5621341013
+                    <span className={styles.cardTextBold}>NIP:</span> 5621341013
                   </p>
                 </div>
                 <hr />
-                <div className="card-info">
-                  <div className="card-img">
+                <div className={styles.cardInfo}>
+                  <div className={styles.cardImg}>
                     <Image
                       src={locationIcon}
                       alt="Adress location icon"
-                      width={32}
-                      height={32}
+                      width={iconSize}
+                      height={iconSize}
                     />
-                    <h5 className="mb-0">Adres</h5>
+                    <h6 className="mb-0 ml-3">Adres</h6>
                   </div>
                   <p>
-                    <span className="text-bold">Żnin</span> 88-400, ul.
-                    700-lecia 32
+                    <span className={styles.cardTextBold}>Żnin 88-400</span>,
+                    ul. 700-lecia 32
                   </p>
                 </div>
                 <hr />
-                <div className="card-info">
-                  <div className="card-img">
+                <div className={styles.cardInfo}>
+                  <div className={styles.cardImg}>
                     <Image
                       src={phoneIcon}
                       alt="Contact phone icon"
-                      width={32}
-                      height={32}
+                      width={iconSize}
+                      height={iconSize}
                     />
-                    <h5 className="mb-0">Kontakt telefoniczny</h5>
+                    <h6 className="mb-0 ml-3">Kontakt telefoniczny</h6>
                   </div>
                   <p>
-                    <span className="text-bold">+48 601-187-677</span>
+                    <span className={styles.cardTextBold}>+48 601-187-677</span>
                   </p>
                   <p>
-                    <span className="text-bold">+48 668-647-677</span>
+                    <span className={styles.cardTextBold}>+48 668-647-677</span>
                   </p>
                 </div>
                 <hr />
-                <div className="card-info">
-                  <div className="card-img">
+                <div className={styles.cardInfo}>
+                  <div className={styles.cardImg}>
                     <Image
                       src={mailIcon}
                       alt="E-mail icon"
-                      width={32}
-                      height={32}
+                      width={iconSize}
+                      height={iconSize}
                     />
-                    <h5>Adres e-mail</h5>
+                    <h6 className="ml-3 mb-0">Adres e-mail</h6>
                   </div>
                   <p>e-mail: artpoz@vp.pl</p>
                 </div>
@@ -112,19 +118,26 @@ export default function Contact() {
 
           <Col lg={7}>
             <div className="mb-4">
-              <div className="card-img">
+              <div className={styles.cardImg}>
                 <Image
                   src={mapIcon}
                   alt="Google maps icon"
-                  width={32}
-                  height={32}
+                  width={48}
+                  height={48}
                 />
-                <span className="text-size text-bold">
-                  <p className="map-text">Tu nas znajdziesz</p>
+                <span className={styles.cardTextBold}>
+                  <h5 className="m-0 p-0 ml-3">TU NAS ZNAJDZIESZ</h5>
                 </span>
               </div>
             </div>
-            <div id="map-container" className="map-container"></div>
+            <div id="map-container" className={mapStyles.mapContainer}>
+              <WrappedMap
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
+                mapElement={defaultMapElement}
+                containerElement={defaultMapElement}
+                loadingElement={defaultMapElement}
+              />
+            </div>
           </Col>
         </Row>
       </Container>
